5dc7d74cb4f0d44643ebdaf7ba2ec894cfd0272a
Mashopy <eliasgheeraert@gmail.com>
2025-06-24 00:27:01 +0200

    misc: mediatek: Fix compilation without MTK_NET_RPS
    Change-Id: I260a77adf05b777e13cfa70100ece95dab2ce825
    Signed-off-by: Frost <frost.github@proton.me>

--- drivers/misc/mediatek/ccmni/ccmni.c
+++ drivers/misc/mediatek/ccmni/ccmni.c
@@ -106,6 +106,7 @@ static void unregister_tcp_pacing_sysctl(void)
 	unregister_sysctl_table(sysctl_header);
 }
 
+#if IS_ENABLED(CONFIG_MTK_NET_RPS)
 void set_ccmni_rps(unsigned long value)
 {
 	int i = 0;
@@ -119,6 +120,7 @@ void set_ccmni_rps(unsigned long value)
 		set_rps_map(ccmni_ctl_blk->ccmni_inst[i]->dev->_rx, value);
 }
 EXPORT_SYMBOL(set_ccmni_rps);
+#endif
 
 void ccmni_set_cur_speed(u64 cur_dl_speed)
 {
@@ -397,7 +399,9 @@ static u16 ccmni_select_queue(struct net_device *dev, struct sk_buff *skb,
 		return CCMNI_TXQ_NORMAL;
 }
 
+#if IS_ENABLED(CONFIG_MTK_NET_RPS)
 static int s_call_times;
+#endif
 
 static int ccmni_open(struct net_device *dev)
 {
@@ -446,9 +450,11 @@ static int ccmni_open(struct net_device *dev)
 		ccmni_ctl_blk->ccci_ops->md_ability,
 		dev->features, gro_flush_timer, ccmni->flt_cnt);
 
+#if IS_ENABLED(CONFIG_MTK_NET_RPS)
 	if (s_call_times == 0)
 		set_ccmni_rps(0x70);
 	s_call_times++;
+#endif
 
 	return 0;
 }
--- drivers/misc/mediatek/usb/usb_rndis/u_ether.c
+++ drivers/misc/mediatek/usb/usb_rndis/u_ether.c
@@ -1048,8 +1048,10 @@ static void eth_start(struct eth_dev *dev, gfp_t gfp_flags)
 {
 	U_ETHER_DBG("\n");
 
+#if IS_ENABLED(CONFIG_MTK_NET_RPS)
 	pr_info("%s - queue_work set_rps_map\n", __func__);
 	queue_work(uether_rps_wq, &dev->rps_map_work);
+#endif
 
 	/* fill the rx queue */
 	rx_fill(dev, gfp_flags);
@@ -1160,6 +1162,7 @@ static int get_ether_addr_str(u8 dev_addr[ETH_ALEN], char *str, int len)
 	return 18;
 }
 
+#if IS_ENABLED(CONFIG_MTK_NET_RPS)
 static void set_rps_map_work(struct work_struct *work)
 {
 	struct eth_dev	*dev = container_of(work, struct eth_dev, rps_map_work);
@@ -1170,6 +1173,7 @@ static void set_rps_map_work(struct work_struct *work)
 	pr_info("%s - set rps to 0xff\n", __func__);
 	set_rps_map(dev->net->_rx, 0xff);
 }
+#endif
 
 static const struct net_device_ops eth_netdev_ops = {
 	.ndo_open		= eth_open,
@@ -1218,7 +1222,9 @@ struct eth_dev *mtk_gether_setup_name(struct usb_gadget *g,
 	INIT_WORK(&dev->work, eth_work);
 	INIT_WORK(&dev->rx_work, process_rx_w);
 	INIT_WORK(&dev->rx_work1, process_rx_w1);
+#if IS_ENABLED(CONFIG_MTK_NET_RPS)
 	INIT_WORK(&dev->rps_map_work, set_rps_map_work);
+#endif
 	INIT_LIST_HEAD(&dev->tx_reqs);
 	INIT_LIST_HEAD(&dev->rx_reqs);
 
@@ -1287,7 +1293,9 @@ struct net_device *mtk_gether_setup_name_default(const char *netname)
 	INIT_WORK(&dev->work, eth_work);
 	INIT_WORK(&dev->rx_work, process_rx_w);
 	INIT_WORK(&dev->rx_work1, process_rx_w1);
+#if IS_ENABLED(CONFIG_MTK_NET_RPS)
 	INIT_WORK(&dev->rps_map_work, set_rps_map_work);
+#endif
 	INIT_LIST_HEAD(&dev->tx_reqs);
 	INIT_LIST_HEAD(&dev->rx_reqs);
 
