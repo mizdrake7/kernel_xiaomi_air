a40afa841665027c0f76740e88e3fce1277e8e3b
Rama Bondan Prakoso <ramanarubp@gmail.com>
2025-11-20 05:05:59 +0700

    lib/lz4: fix -Wdeclaration-after-statement build error
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>

--- lib/lz4/lz4.c
+++ lib/lz4/lz4.c
@@ -1773,6 +1773,7 @@ int LZ4_compress_destSize_extState(void *state, const char *src, char *dst,
 int LZ4_compress_destSize(const char *src, char *dst, int *srcSizePtr,
 			  int targetDstSize)
 {
+	int result;
 #if (LZ4_HEAPMODE)
 	LZ4_stream_t *const ctx = (LZ4_stream_t *)ALLOC(sizeof(
 		LZ4_stream_t)); /* malloc-calloc always properly aligned */
@@ -1783,7 +1784,7 @@ int LZ4_compress_destSize(const char *src, char *dst, int *srcSizePtr,
 	LZ4_stream_t *const ctx = &ctxBody;
 #endif
 
-	int result = LZ4_compress_destSize_extState_internal(
+	result = LZ4_compress_destSize_extState_internal(
 		ctx, src, dst, srcSizePtr, targetDstSize, 1);
 
 #if (LZ4_HEAPMODE)
