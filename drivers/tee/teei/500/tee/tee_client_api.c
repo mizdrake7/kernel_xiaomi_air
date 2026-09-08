3c1250cbc2450393838d3e59f8c9e40d41c61e00
exerczz1 <exrcz513@gmail.com>
2026-09-08 12:18:27 +0600

    drivers/tee/teei: allocate buffer on heap in TEEC_OpenSession and TEEC_InvokeCommand
    TEEC_OpenSession and TEEC_InvokeCommand allocated large buffers
    alongside 192-byte shm arrays directly on the kernel stack. This pushed
    the stack frame size to 528 bytes, exceeding the single AArch64 pre-index
    offset limit of 512 bytes.
    
    Under Clang 11, this caused prologue splitting where the stack canary
    and buffer zeroing were written below SP before the second stack decrement,
    leaving them vulnerable to being clobbered by EL1 IRQ exception frames.
    Additionally, passing kernel stack buffers to in-kernel ioctls with uaccess
    is unsafe.
    
    Allocate buf dynamically with kzalloc(..., GFP_KERNEL) and free it on
    exit with kfree() to keep the stack frame small and safe.
    
    Signed-off-by: exerczz1 <exrcz513@gmail.com>

--- drivers/tee/teei/500/tee/tee_client_api.c
+++ drivers/tee/teei/500/tee/tee_client_api.c
@@ -467,10 +467,10 @@ TEEC_Result TEEC_OpenSession(struct TEEC_Context *ctx,
 			struct TEEC_Operation *operation,
 			uint32_t *ret_origin)
 {
-	uint64_t buf[(sizeof(struct tee_ioctl_open_session_arg) +
+	const size_t buf_len = sizeof(struct tee_ioctl_open_session_arg) +
 			TEEC_CONFIG_PAYLOAD_REF_COUNT *
-				sizeof(struct tee_ioctl_param)) /
-			sizeof(uint64_t)] = { 0 };
+				sizeof(struct tee_ioctl_param);
+	void *buf;
 	struct tee_ioctl_buf_data buf_data;
 	struct tee_ioctl_open_session_arg *arg;
 	struct tee_ioctl_param *params;
@@ -487,8 +487,15 @@ TEEC_Result TEEC_OpenSession(struct TEEC_Context *ctx,
 		goto out;
 	}
 
+	buf = kzalloc(buf_len, GFP_KERNEL);
+	if (!buf) {
+		eorig = TEEC_ORIGIN_API;
+		res = TEEC_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
 	buf_data.buf_ptr = (uintptr_t)buf;
-	buf_data.buf_len = sizeof(buf);
+	buf_data.buf_len = buf_len;
 
 	arg = (struct tee_ioctl_open_session_arg *)buf;
 	arg->num_params = TEEC_CONFIG_PAYLOAD_REF_COUNT;
@@ -520,6 +527,7 @@ TEEC_Result TEEC_OpenSession(struct TEEC_Context *ctx,
 
 out_free_temp_refs:
 	teec_free_temp_refs(operation, shm);
+	kfree(buf);
 out:
 	if (ret_origin)
 		*ret_origin = eorig;
@@ -544,10 +552,10 @@ EXPORT_SYMBOL(TEEC_CloseSession);
 TEEC_Result TEEC_InvokeCommand(struct TEEC_Session *session, uint32_t cmd_id,
 		struct TEEC_Operation *operation, uint32_t *error_origin)
 {
-	uint64_t buf[(sizeof(struct tee_ioctl_invoke_arg) +
+	const size_t buf_len = sizeof(struct tee_ioctl_invoke_arg) +
 			TEEC_CONFIG_PAYLOAD_REF_COUNT *
-				sizeof(struct tee_ioctl_param)) /
-			sizeof(uint64_t)] = { 0 };
+				sizeof(struct tee_ioctl_param);
+	void *buf;
 	struct tee_ioctl_buf_data buf_data;
 	struct tee_ioctl_invoke_arg *arg;
 	struct tee_ioctl_param *params;
@@ -562,8 +570,15 @@ TEEC_Result TEEC_InvokeCommand(struct TEEC_Session *session, uint32_t cmd_id,
 		goto out;
 	}
 
+	buf = kzalloc(buf_len, GFP_KERNEL);
+	if (!buf) {
+		eorig = TEEC_ORIGIN_API;
+		res = TEEC_ERROR_OUT_OF_MEMORY;
+		goto out;
+	}
+
 	buf_data.buf_ptr = (uintptr_t)buf;
-	buf_data.buf_len = sizeof(buf);
+	buf_data.buf_len = buf_len;
 
 	arg = (struct tee_ioctl_invoke_arg *)buf;
 	arg->num_params = TEEC_CONFIG_PAYLOAD_REF_COUNT;
@@ -598,6 +613,7 @@ TEEC_Result TEEC_InvokeCommand(struct TEEC_Session *session, uint32_t cmd_id,
 
 out_free_temp_refs:
 	teec_free_temp_refs(operation, shm);
+	kfree(buf);
 out:
 	if (error_origin)
 		*error_origin = eorig;
